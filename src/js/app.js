974b08065a62c67701b39cbef42a9d9ba2cf9c6b
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2023-06-12 13:48:59 +0900

    조명 주석 처리 + meshstandardmaterial로 변화 - >검정색으로 변함
    이유) meshstandardmaterial은 pbr 즉, 물리기반렌더링이므로, 조명 조건이 필요하다!

--- src/js/app.js
+++ src/js/app.js
@@ -32,10 +32,10 @@ export default function () {
 
   const createEarth1 = () => {
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-    scene.add(ambientLight);
+   // const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+   // scene.add(ambientLight);
     //지구에 적용할 푸른색상이 머테리얼을 만든다.
-    const material = new THREE.MeshBasicMaterial({map:textureLoader.load('assets/earth_nightmap.jpg')});
+    const material = new THREE.MeshStandardMaterial({map:textureLoader.load('assets/earth_nightmap.jpg')});
     //이렇게 하면 자동으로 public 폴도 밑의 assets 폴더로 
     //지구처럼 둥근 sphere geometry 만든다. 
     const geometry = new THREE.SphereGeometry(1.3,30,30);
